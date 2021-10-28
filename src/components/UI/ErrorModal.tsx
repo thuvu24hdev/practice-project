9dba6a06c49d2b44d2dcf5bd3f58c6148c4f47d4
VuThiBichThu <thuvu.24hdev@gmail.com>
2021-10-28 16:58:29 +0700

    working with portals

--- src/components/UI/ErrorModal.tsx
+++ src/components/UI/ErrorModal.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import Card from "./Card";
 import Button from "./Button";
 import classes from "./ErrorModal.module.css";
+import ReactDOM from "react-dom";
 
 interface ErrorModalProps {
   onConfirm: () => void;
@@ -10,21 +11,45 @@ interface ErrorModalProps {
   message: string;
 }
 
+interface BackdropProps {
+  onConfirm: () => void;
+}
+
+const Backdrop = (props: BackdropProps) => {
+  return <div className={classes.backdrop} onClick={props.onConfirm} />;
+};
+
+const ModalOverlay = (props: ErrorModalProps) => {
+  return (
+    <Card className={classes.modal}>
+      <header className={classes.header}>
+        <h2>{props.title}</h2>
+      </header>
+      <div className={classes.content}>
+        <p>{props.message}</p>
+      </div>
+      <footer className={classes.actions}>
+        <Button onClick={props.onConfirm}>Okay</Button>
+      </footer>
+    </Card>
+  );
+};
+
 const ErrorModal = (props: ErrorModalProps) => {
   return (
     <>
-      <div className={classes.backdrop} onClick={props.onConfirm} />
-      <Card className={classes.modal}>
-        <header className={classes.header}>
-          <h2>{props.title}</h2>
-        </header>
-        <div className={classes.content}>
-          <p>{props.message}</p>
-        </div>
-        <footer className={classes.actions}>
-          <Button onClick={props.onConfirm}>Okay</Button>
-        </footer>
-      </Card>
+      {ReactDOM.createPortal(
+        <Backdrop onConfirm={props.onConfirm} />,
+        document.getElementById("backdrop-root") as Element
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay
+          title={props.title}
+          message={props.message}
+          onConfirm={props.onConfirm}
+        />,
+        document.getElementById("overlay-root") as Element
+      )}
     </>
   );
 };
